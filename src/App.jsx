f99fdf1c3c4214607490250c189036e50a115495
shakil218 <rabiulhasanshakil218@gmail.com>
2026-03-29 23:15:11 +0600

    project setup and initial package install done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <h1 class="bg-linear-to-bl from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent text-3xl font-bold text-center">
+  Gradient Text
+</h1>
+    </>
+  )
+}
+
+export default App
